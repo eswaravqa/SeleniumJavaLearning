f32ecbefcfc43a2b81107ee809ebde2475e4ba3f
eswaravqa <eswarav.qa@gmail.com>
2023-12-19 10:38:33 -0500

    Modified BestBuySite and FunctionToClickOnElement files and added cartpage

---
+++ src/main/java/io/com/qa/basics/selenium/cartpage.java
@@ -0,0 +1,11 @@
+package io.com.qa.basics.selenium;
+
+public class cartpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Place holder class to work on cartpage items");
+
+	}
+
+}
